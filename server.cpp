6a4397e20e8118f4a52f8469aab235271cc4a572
OhssineAbdelfatah <abdelfatah.ohssine@gmail.com>
2025-08-12 23:20:18 +0100

    req/res works in a basic way

--- server.cpp
+++ server.cpp
@@ -10,54 +10,63 @@
 
 #define PORT 8080
 
-int main() 
+int main()
 {
-  int server_fd, new_socket;
-  struct sockaddr_in address;
-  int opt = 1;
-  int addrlen = sizeof(address);
-  char buffer[1024] = {0};
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char writeBuffer[] = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!\n";
+    char readBuffer[3000] ;
+    // Create socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
 
-  // Create socket
-  server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd == 0) {
-    perror("socket failed");
-    exit(EXIT_FAILURE);
-  }
+    // Attach socket to the port
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)); // explain
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-  // Attach socket to the port
-  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));// explain
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(PORT);
+    // Bind
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
 
-  // Bind
-  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
-  }
+    // Listen
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    while (1)
+    {
 
-  // Listen
-  if (listen(server_fd, 3) < 0) {
-    perror("listen");
-    exit(EXIT_FAILURE);
-  }
+        // Accept a connection
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        int new_socket;
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
 
-  // Accept a connection
-  new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-  if (new_socket < 0) {
-    perror("accept");
-    exit(EXIT_FAILURE);
-  }
+        // Read data
+        read(new_socket, readBuffer, 3000);
+        std::cout << "Message from client: " << readBuffer << std::endl;
+        write(new_socket, writeBuffer, strlen(writeBuffer));
 
-  // Read data
-  read(new_socket, buffer, 1024);
-  std::cout << "Message from client: " << buffer << std::endl;
-
-  // Close socket
-  close(new_socket);
-  close(server_fd);
-  return 0;
+        // Close socket
+        close(new_socket);
+    }
+    close(server_fd);
+    return 0;
 }
 /*
   this program accept one request and exit
